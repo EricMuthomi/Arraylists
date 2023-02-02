ef724ff990813d51ed1d5c5d449ce5daf4751d28
Eric Palmer <muthomiericpalmer@gmail.com>
2023-02-02 19:03:44 +0300

    Updated Arrays

--- src/PlaylistArraylist.java
+++ src/PlaylistArraylist.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PlaylistArraylist {
    public static void main(String[] args) {
@@ -35,6 +36,26 @@ public class PlaylistArraylist {
 
 //print out the playlist; it should show the new order
             System.out.println(desertIslandPlaylist);
+
+            //initialize the Array object
+        MyArrays myArrays = new MyArrays();
+        int myArr[] = myArrays.createArray();
+        System.out.println("The newly created array is: "+Arrays.toString(myArr));
+
+        myArray();
+        }
+
+        public static void myArray(){
+            String[] studentNames = new String[3];
+            studentNames[0] = "Eric";
+            studentNames[1] = "Anonymous";
+            studentNames[2] = "Palmer";
+
+            int[] numbers ={10, 17, 7, 11, 33};
+            Arrays.sort(numbers);
+
+            System.out.println("My array of names: "+Arrays.deepToString(studentNames));
+            System.out.println("My sorted array of numbers is: "+Arrays.toString(numbers));
         }
 
     }
